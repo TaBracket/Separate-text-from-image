824f4e0c6816b05ac50dd1396d2410ad12c0ef20
majid hatami maleki <majidhatamimaleki@outlook.com>
2025-10-18 10:14:05 +0330

    a

--- detect-text/app.py
+++ detect-text/app.py
@@ -1,13 +1,19 @@
 import cv2
 import os
+# Load Image
+# import matplotlib for viz
 from matplotlib import pyplot as plt
-IMG_PATH = os.path.join('images', '1.jpg')
+# define the file path to the image
+IMG_PATH = os.path.join("images", "1.jpg")
+# Read in image
 img = cv2.imread(IMG_PATH)
-print(img)
-# resize img
-resized_img = cv2.resize(img,(int(img.shape[1]/8),int(img.shape[0]/8)))
-# view image using openCv
-cv2.imshow('frame view', resized_img)
+# Resize image
+resized_img = cv2.resize(img , (int(img.shape[1]/4) , int(img.shape[0]/4)))
+# Edge detection using Canny
+gray = cv2.cvtColor(resized_img,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray,(5,5),0 )
+canny = cv2.Canny(blur, threshold1=100, threshold2=100)
+# view image using opencv
+cv2.imshow('img',canny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-print("Image shape:", img.shape)
